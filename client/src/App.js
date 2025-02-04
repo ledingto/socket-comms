98b537ce2953360cb8b1a657574b1877fea66f0d
Laura Edington <ledingto@users.noreply.github.com>
2025-02-03 16:39:19 -0800

    Create Client component to connect multiple clients with unique customer ids

--- client/src/App.js
+++ client/src/App.js
@@ -6,35 +6,34 @@ import './App.css';
 const SOCKET_URL = "http://localhost:3005/orders";
 const API_BASE_URL = "/api";
 
-const socket = io(SOCKET_URL, {
-  transports: ['websocket']
-});
-
-function App() {
+/* Reusable client component that handles it's own socket connection to
+ * simulate multiple customers receiving updates from the server
+ */
+function Client({ customerId }) {
   const [orders, setOrders] = useState([]);
-  const CUSTOMER_ID = "123"; // In real app, this would come from authorization
 
   useEffect(() => {
+    // FETCH ORDERS
     const fetchOrders = async () => {
       try {
-        const response = await fetch(`${API_BASE_URL}/get-orders/${CUSTOMER_ID}`);
+        const response = await fetch(`${API_BASE_URL}/get-orders/${customerId}`);
         const data = await response.json();
         if (data.success) {
           setOrders(data.customerOrders);
         }
       } catch (error) {
-        console.error('Error fetching orders:', error);
+        console.error(`Error fetching orders for Client ${customerId}:`, error);
       }
     };
     fetchOrders();
 
     // Subscribe to customer updates
-    console.log("Subscribing to customer updates");
-    socket.emit('subscribeToCustomer', CUSTOMER_ID);
+    console.log(`Client ${customerId} subscribing to order updates`);
+    socket.emit('subscribeToCustomer', customerId);
     
     // Add listener for order updates
     socket.on('messageFromServer', (data) => {
-      console.log("Received order update:", data);
+      console.log(`Client ${customerId} received order update:`, data);
       setOrders(prevOrders =>
         prevOrders.map(order =>
           order.order_id === data.orderId
@@ -46,14 +45,25 @@ function App() {
 
     return () => {
       socket.off('messageFromServer');
+      // socket.disconnect();
     };
   }, []);
 
   return (
+    <>
+      <p>Customer ID: {customerId}</p>
+      <OrderTable orders={orders} />
+    </>
+  )
+}
+
+function App() {
+  return (
     <div className="container">
       <h1>WebSocket with Socket.io</h1>
-      <p>Customer ID: {CUSTOMER_ID}</p>
-      <OrderTable orders={orders} />
+      <Client customerId="123" />
+      <Client customerId="456" />
+      <Client customerId="789" />
     </div>
   );
 }
